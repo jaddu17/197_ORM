f0f95da12f09455fec00b8c1017d8b7787912f80
jaddu17 <shafajaddu@gmail.com>
2025-10-28 14:28:07 +0700

    komik.js

---
+++ models/komik.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        judul : {
+            type : DataTypes.STRING,
+            allowNull : false
+        },
+        penulis : {
+            type : DataTypes.STRING,
+            allowNull : false
+        },
+        deskripsi : {
+            type : DataTypes.TEXT,
+            allowNull : false
+        }
+    }, {
+        tableName : 'komik',
+        timestamps : true,
+        freezeTableName : true
+    });
+    return Komik;
+}
